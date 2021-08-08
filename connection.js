559dd95b9d3957566d20611fff677fc2f0853945
Zainab Faizan Khan <zainabfaian712@gmail.com>
2021-08-08 23:45:56 +0500

    uhggh

--- connection.js
+++ connection.js
@@ -35,6 +35,13 @@ app.use(session({
       maxAge: 8*60*60*1000, // 8hr
   }
 }));
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://touristooze.herokuapp.com`)
+    } else {
+      next();
+    }
+  });
 app.use(express.static(publicDirectory));
 
 
